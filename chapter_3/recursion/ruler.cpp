a2a158df541ea4395faa6fd1725b8ea8a08692c0
qaz9517532846 <qaz9517532846@outlook.com>
2025-10-04 18:06:19 +0800

    Update ruler.cpp

--- chapter_3/recursion/ruler.cpp
+++ chapter_3/recursion/ruler.cpp
@@ -2,20 +2,30 @@
 
 using namespace std;
 
-int main(void) {
-
-}
-
-
 void drawOneTick(int tickLength, int tickLabel = -1) {
     for (int i = 0; i < tickLength; i++)
         cout << "-";
-    if (tickLael >= 0) cout << " " << tickLabel;
+    if (tickLabel >= 0) cout << " " << tickLabel;
     cout << "\n";
 }
 
 void drawTicks(int tickLength) {
     if (tickLength > 0) {
-        drawTicks
+        drawTicks(tickLength - 1);
+        drawOneTick(tickLength);
+        drawTicks(tickLength - 1);
     }
+}
+
+void drawRuler(int nInches, int majorLength) {
+    drawOneTick(majorLength, 0);
+    for(int i = 1; i <= nInches; i++) {
+        drawTicks(majorLength - 1);
+        drawOneTick(majorLength, i);
+    }
+}
+
+int main(void) {
+    drawRuler(2, 4);
+    return EXIT_SUCCESS;
 }
